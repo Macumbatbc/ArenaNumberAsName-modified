9352f2d25bbfc2c2c8c079d1f78465b076c6e3b6
Macumba <85767653+Macumbatbc@users.noreply.github.com>
2022-02-02 00:52:36 +0100

    Fix for 2.5.3 patch
    Made with the code of Xyz !

--- ArenaNumberAsName.lua
+++ ArenaNumberAsName.lua
@@ -1,37 +1,38 @@
+local CF = CreateFrame("Frame")
+CF:RegisterEvent("ADDON_LOADED")
+
 local U=UnitIsUnit
+
 local function NameToArenaNumber(frame, unit)
 	if select(1, IsActiveBattlefieldArena()) and UnitIsEnemy("player", unit) and UnitPlayerControlled(unit) then
 		for i=1,5 do 
-			if U(unit,"arena"..i) then
+			if U(unit, "arena"..i) then
 				local unitClass = UnitClass(unit)
 				if string.find(frame.unit, "nameplate") then
 					frame.name:SetText(unitClass)
 					frame.name:SetFont("Fonts\\FRIZQT__.TTF", 10, "OUTLINE")
-					frame.name:SetTextColor(255,255,255)
+					frame.name:SetTextColor(0,1,1)
 				else
 					frame.name:SetText(unitClass)
 				end
 			end
 		end
 	end
+	CF:UnregisterEvent("ADDON_LOADED")
 end
 
-hooksecurefunc("ArenaEnemyFrame_UpdatePlayer", function(self)
-	local unit = self.unit
+CF:SetScript("OnEvent", function(self, event, ...)
+	if event == "ADDON_LOADED" then
+		hooksecurefunc("CompactUnitFrame_UpdateName",function(frame)
+			local unit = frame.unit
 	
-	if self.name then
-		NameToArenaNumber(self, unit)
-	end
-end)
+			if not select(1, IsActiveBattlefieldArena()) and string.find(frame.unit, "nameplate") and not frame:IsForbidden() then
+				frame.name:SetFont("Fonts\\FRIZQT__.TTF", 12)
+			end
 
-hooksecurefunc("CompactUnitFrame_UpdateName",function(frame)
-	local unit = frame.unit
-	
-	if not select(1, IsActiveBattlefieldArena()) and not frame:IsForbidden() then
-		frame.name:SetFont("Fonts\\FRIZQT__.TTF", 12)
+			NameToArenaNumber(frame, unit)
+		end)
 	end
-
-	NameToArenaNumber(frame, unit)
 end);
 
 hooksecurefunc("TargetFrame_Update", function(self)
@@ -50,22 +51,3 @@ TargetFrameToT:HookScript("OnUpdate", function(self, elapsed)
 		NameToArenaNumber(self, unit)
 	end
 end);
-
-GameTooltip:HookScript("OnTooltipSetUnit", function(GameTooltip)
-	local _, unit = GameTooltip:GetUnit()
-	if UnitIsPlayer(unit) then
-		local _, class = UnitClass(unit)
-		local color = class and (CUSTOM_CLASS_COLORS or RAID_CLASS_COLORS)[class]
-		if color then
-			local text = GameTooltipTextLeft1:GetText()
-			if select(1, IsActiveBattlefieldArena()) and UnitIsEnemy("player", unit) and UnitPlayerControlled(unit) then
-				for i=1,5 do 
-					if U(unit,"arena"..i) then
-						text = "Arena: " .. i;
-					end
-				end
-			end
-			GameTooltipTextLeft1:SetFormattedText("|cff%02x%02x%02x%s|r", color.r * 255, color.g * 255, color.b * 255, text:match("|cff\x\x\x\x\x\x(.+)|r") or text)
-		end
-	end
-end)
